888cd29c624c205ed7bcc0e58eee81dba6ff4dee
Qunainb <waniqunain@gmail.com>
2025-04-05 07:07:04 +0530

    useCallback function to stop infinite loop

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -11,7 +11,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
       console.log("TIMER Stopped");
       clearTimeout(timer);
     };
-  }, []);
+  }, [onConfirm]);
 
   return (
     <div id="delete-confirmation">
